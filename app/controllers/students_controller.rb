09f0d99a4dbca245a5b1eb58478705f5f96a19e9
Maryann Mwikali <mwikali119@gmail.com>
2022-12-24 23:48:01 +0300

    Done - bonus tests not yet implemem=nted

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
-
-  def index
+  def to_query(namespace = nil)
+    collect do |key, value|
+      value.to_query(namespace ? "#{namespace}[#{key}]" : key)
+    end.sort * ''
+  end
+    
+  def index()
     students = Student.all
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
